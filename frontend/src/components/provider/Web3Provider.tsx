19a771f60f6a0ae13487f5e4733d9890d78a4a58
Atulya Minz <atulminz.jsr@gmail.com>
2025-08-13 23:52:36 +0530

    Added provider

---
+++ frontend/src/components/provider/Web3Provider.tsx
@@ -0,0 +1,4 @@
+import { WagmiProvider, createConfig, http } from "wagmi";
+import {} from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
