ca61bf02ad7da95cbfcb2dc8ddb893d9d8f951e6
Atulya Minz <atulminz.jsr@gmail.com>
2025-08-17 23:34:46 +0530

    updated

--- frontend/src/components/provider/Web3Provider.tsx
+++ frontend/src/components/provider/Web3Provider.tsx
@@ -1,4 +1,32 @@
 import { WagmiProvider, createConfig, http } from "wagmi";
-import {} from "wagmi/chains";
+import { baseSepolia, sepolia } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import type React from "react";
+
+const config = createConfig(
+  getDefaultConfig({
+    chains: [sepolia, baseSepolia],
+    transports: {
+      [sepolia.id]: http(),
+    },
+
+    walletConnectProjectId: "",
+    appName: "",
+    appDescription: "",
+    appUrl: "",
+    appIcon: "",
+  })
+);
+
+const queryClient = new QueryClient();
+
+export const Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
